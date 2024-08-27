2ef8b6518f8acafa009d9dd7dbdfdf6537874c7f
tirtha-das <tdas86577@gmail.com>
2024-08-27 11:40:22 +0530

    Episode 01

---
+++ index.js
@@ -0,0 +1,14 @@
+let root = ReactDOM.createRoot(document.querySelector('.container'));
+
+let parent = React.createElement('div',{id:"parent"},[
+    React.createElement('div',{id:"child1"},[
+        React.createElement('h1',{},`I'm h1 tag`),
+        React.createElement('h2',{},`I'm h2 tag`),
+    ]),
+    React.createElement('div',{id:"child2"},[
+        React.createElement('h1',{},`I'm h1 tag`),
+        React.createElement('h2',{},`I'm h2 tag`),
+    ])
+]);
+
+root.render(parent);
