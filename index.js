cef540726f5b41afce0d1a084b097610a72292a4
tirtha-das <tdas86577@gmail.com>
2024-08-28 13:00:00 +0530

    commits

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ import ReactDOM from "react-dom";
 
 let root = ReactDOM.createRoot(document.querySelector('.container'));
 
-let parent = React.createElement('div',{id:"parent"},[
-    React.createElement('div',{id:"child1"},[
-        React.createElement('h1',{},`I'm h1 tag`),
-        React.createElement('h2',{},`I'm h2 tag`),
-    ]),
-    React.createElement('div',{id:"child2"},[
-        React.createElement('h1',{},`I'm h1 tag`),
-        React.createElement('h2',{},`I'm h2 tag`),
-    ])
-]);
+// let parent = React.createElement('div',{id:"parent"},[
+//     React.createElement('div',{id:"child1"},[
+//         React.createElement('h1',{},`I'm h1 tag`),
+//         React.createElement('h2',{},`I'm h2 tag`),
+//     ]),
+//     React.createElement('div',{id:"child2"},[
+//         React.createElement('h1',{},`I'm h1 tag`),
+//         React.createElement('h2',{},`I'm h2 tag`),
+//     ])
+// ]);
+
+let parent = <h1>hello guys<spam>I am in spam</spam></h1>
 
 root.render(parent);
